d7c1adefa399a40fec1e7dfd9f9ce3dde6ce52f0
Andres Aguilar <aaguil3@gmail.com>
2020-05-13 19:27:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ min_array = []
 row_index = 0 
 while row_index < src.count do
   element_index = 0 
-  shortest_string_integer = src[count][0]
+  shortest_string_integer = src[row_index][0]
   while element_index < src[row_index].count do
     if src[row_index][element_index] < shortest_string_integer
       shortest_string_integer = src[row_index][element_index]
